b9ce68ad39d12eb1bec4c983c03fd66bdbc476c7
Shin HyunHo <newadda@live.com>
2022-08-03 14:50:15 +0900

    소스 추가

--- spring-template/src/main/java/org/onecell/spring/template/WebApplication.java
+++ spring-template/src/main/java/org/onecell/spring/template/WebApplication.java
@@ -35,24 +35,26 @@ public class WebApplication extends SpringBootServletInitializer implements Appl
 
     }
 
-    //// war 로 외부 Tomcat 에서 실행 될때 사용할 수 있다.
+    //// war 로 외부 Tomcat 에서 실행 될때 시작점이다.
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 
         /// ==== 프로젝트명 변경 =====
         /// 기본적으로 읽는 properties 파일 이름을 프로젝트명등으로 바꿀수 있다.
         /// 예를 들어 기본은 application.properties 이지만 app-test.properteis 로 바꿀수 있다.
-        //return builder.sources(WebApplication.class).properties("spring.config.name: "+ CONFIG_NAME);
+        //return builder.sources(WebApplication.class).properties("spring.config.name: "+ "app-test");
         /// ========================
 
         return super.configure(builder);
     }
 
+    //// 내장 웹서버 시작시 시작점이다.
     public static void main(String[] args) throws IOException {
 
         /// ==== 프로젝트명 변경 =====
         /// 내장 톰캣 사용시 기본적으로 읽는 properties 파일 이름을 프로젝트명등으로 바꿀수 있다.
-        //System.setProperty("spring.config.name", CONFIG_NAME);
+        /// 예를 들어 기본은 application.properties 이지만 app-test.properteis 로 바꿀수 있다.
+        //System.setProperty("spring.config.name",  "app-test");
         /// ========================
 
 
